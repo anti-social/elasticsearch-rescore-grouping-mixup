19b53667a75be47c67f1cb51d46e542869a1b0d0
Alexander Koval <kovalidis@gmail.com>
2018-04-11 17:05:51 +0300

    Added version to the artifact

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,15 @@
+import org.elasticsearch.gradle.VersionProperties
+
 buildscript {
+    val esVersion = "6.2.3"
+
     repositories {
         mavenCentral()
         jcenter()
     }
     dependencies {
-        classpath("org.elasticsearch.gradle:build-tools:6.2.3")
+        classpath("org.elasticsearch.gradle:build-tools:$esVersion")
     }
-}
-
-repositories {
-    mavenCentral()
-    jcenter()
 }
 
 plugins {
@@ -22,12 +21,24 @@ apply {
     plugin("elasticsearch.esplugin")
 }
 
+val pluginVersion = "0.1.0-SNAPSHOT"
+        .let { ver ->
+            if (hasProperty("release")) {
+                ver.removeSuffix("-SNAPSHOT")
+            } else {
+                ver
+            }
+        }
+
 configure<org.elasticsearch.gradle.plugin.PluginPropertiesExtension> {
     name = "rescore-grouping-mixup"
     description = "Adds rescorer for mixing up search hits inside their groups."
-    version = "0.1.0"
     classname = "company.evo.elasticsearch.plugin.GroupingMixupPlugin"
+    version = pluginVersion
 }
 
 project.setProperty("licenseFile", project.rootProject.file("LICENSE.txt"))
 project.setProperty("noticeFile", project.rootProject.file("NOTICE.txt"))
+
+val versions = VersionProperties.getVersions() as Map<String, String>
+project.version = "$pluginVersion-es${versions["elasticsearch"]}"
