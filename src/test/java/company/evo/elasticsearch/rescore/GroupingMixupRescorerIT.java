4ed1069090ca4b88613ec53147ef61b6ca419985
Alexander Koval <kovalidis@gmail.com>
2018-04-11 21:57:56 +0300

    Split integration tests

--- src/test/java/company/evo/elasticsearch/rescore/GroupingMixupRescorerIT.java
+++ src/test/java/company/evo/elasticsearch/rescore/GroupingMixupRescorerIT.java
@@ -31,8 +31,8 @@ import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.script.Script;
 import org.elasticsearch.script.ScriptType;
+import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.test.ESIntegTestCase;
-import org.elasticsearch.search.rescore.RescorePhase;
 
 import static org.elasticsearch.cluster.metadata.IndexMetaData.SETTING_NUMBER_OF_SHARDS;
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
@@ -43,6 +43,9 @@ import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertOrde
 @ESIntegTestCase.ClusterScope(scope = ESIntegTestCase.Scope.SUITE)
 public class GroupingMixupRescorerIT extends ESIntegTestCase {
     private static final String DEBUG_SEP = "======================";
+
+    private static final MatchQueryBuilder queryBuilder = QueryBuilders
+            .matchQuery("name", "the quick brown");
 
     public void testEmptyIndex() throws IOException {
         assertAcked(prepareCreate("test")
@@ -76,66 +79,27 @@ public class GroupingMixupRescorerIT extends ESIntegTestCase {
         assertHitCount(resp, 0);
     }
 
-    public void testRescoring() throws IOException {
-        assertAcked(prepareCreate("test")
-                .setSettings(Settings.builder().put(SETTING_NUMBER_OF_SHARDS, 1))
-                .addMapping("product",
-                        jsonBuilder().startObject().startObject("product").startObject("properties")
-                        .startObject("name")
-                                .field("type", "text")
-                                .field("analyzer", "whitespace")
-                        .endObject()
-                        .startObject("company_id")
-                                .field("type", "integer")
-                        .endObject()
-                        .endObject().endObject().endObject()));
+    public void testNoRescoring() throws IOException {
+        createIndexAndPopulateDocs();
 
-        client().prepareIndex("test", "product", "1")
-            .setSource(
-                    "name", "the quick brown fox",
-                    "company_id", 1)
-            .execute()
-            .actionGet();
-        client().prepareIndex("test", "product", "2")
-            .setSource(
-                    "name", "the quick lazy huge fox jumps over the tree",
-                    "company_id", 2)
-            .execute()
-            .actionGet();
-        client().prepareIndex("test", "product", "3")
-            .setSource(
-                    "name", "quick huge brown fox",
-                    "company_id", 1)
-            .execute()
-            .actionGet();
-        client().prepareIndex("test", "product", "4")
-            .setSource("name", "the quick lonely fox")
-            .execute()
-            .actionGet();
-        ensureYellow();
-        refresh();
-
-        SearchResponse resp;
-        MatchQueryBuilder queryBuilder = QueryBuilders
-                .matchQuery("name", "the quick brown");
-
-        // No rescore
-        resp = client().prepareSearch()
+        SearchResponse resp = client().prepareSearch()
                 .setQuery(queryBuilder)
                 .execute()
                 .actionGet();
         assertHitCount(resp, 4);
-        org.elasticsearch.search.SearchHits hits;
-        hits = resp.getHits();
+        SearchHits hits = resp.getHits();
         logger.info("{} {}", hits.getAt(0).getId(), hits.getAt(0).getScore());
         logger.info("{} {}", hits.getAt(1).getId(), hits.getAt(1).getScore());
         logger.info("{} {}", hits.getAt(2).getId(), hits.getAt(2).getScore());
         logger.info("{} {}", hits.getAt(3).getId(), hits.getAt(3).getScore());
         logger.info(DEBUG_SEP);
         assertOrderedSearchHits(resp, "1", "3", "4", "2");
+    }
 
-        // With rescoring
-        resp = client().prepareSearch()
+    public void testRescoringAllDocs() throws IOException {
+        createIndexAndPopulateDocs();
+
+        SearchResponse resp = client().prepareSearch()
                 .setQuery(queryBuilder)
                 .setRescorer(
                         new GroupingMixupRescorerBuilder(
@@ -145,25 +109,28 @@ public class GroupingMixupRescorerIT extends ESIntegTestCase {
                                         "grouping_mixup_scripts",
                                         "position_recip",
                                         Collections.emptyMap()))
-                        .windowSize(5))
+                                .windowSize(5))
                 .execute()
                 .actionGet();
         assertHitCount(resp, 4);
-        hits = resp.getHits();
+        SearchHits hits = resp.getHits();
         logger.info("{} {}", hits.getAt(0).getId(), hits.getAt(0).getScore());
         logger.info("{} {}", hits.getAt(1).getId(), hits.getAt(1).getScore());
         logger.info("{} {}", hits.getAt(2).getId(), hits.getAt(2).getScore());
         logger.info("{} {}", hits.getAt(3).getId(), hits.getAt(3).getScore());
         logger.info(DEBUG_SEP);
         assertOrderedSearchHits(resp, "1", "4", "2", "3");
+    }
 
-        // Rescoring with script params
+    public void testRescoringWithCustomScriptParams() throws IOException {
+        createIndexAndPopulateDocs();
+
         Map<String, Object> scriptParams = new HashMap<>();
         scriptParams.put("m", 1.0);
         scriptParams.put("a", 1.0);
         scriptParams.put("b", 1.1);
         scriptParams.put("c", 0.090909);
-        resp = client().prepareSearch()
+        SearchResponse resp = client().prepareSearch()
                 .setQuery(queryBuilder)
                 .setRescorer(
                         new GroupingMixupRescorerBuilder(
@@ -173,20 +140,23 @@ public class GroupingMixupRescorerIT extends ESIntegTestCase {
                                         "grouping_mixup_scripts",
                                         "position_recip",
                                         scriptParams))
-                    .windowSize(5))
+                                .windowSize(5))
                 .execute()
                 .actionGet();
         assertHitCount(resp, 4);
-        hits = resp.getHits();
+        SearchHits hits = resp.getHits();
         logger.info("{} {}", hits.getAt(0).getId(), hits.getAt(0).getScore());
         logger.info("{} {}", hits.getAt(1).getId(), hits.getAt(1).getScore());
         logger.info("{} {}", hits.getAt(2).getId(), hits.getAt(2).getScore());
         logger.info("{} {}", hits.getAt(3).getId(), hits.getAt(3).getScore());
         logger.info(DEBUG_SEP);
         assertOrderedSearchHits(resp, "1", "4", "3", "2");
+    }
 
-        // Small size
-        resp = client().prepareSearch()
+    public void testRescoringWithSmallSize() throws IOException {
+        createIndexAndPopulateDocs();
+
+        SearchResponse resp = client().prepareSearch()
                 .setQuery(queryBuilder)
                 .setSize(2)
                 .setRescorer(
@@ -197,17 +167,21 @@ public class GroupingMixupRescorerIT extends ESIntegTestCase {
                                         "grouping_mixup_scripts",
                                         "position_recip",
                                         Collections.emptyMap()))
-                        .windowSize(5))
+                                .windowSize(5))
                 .execute()
                 .actionGet();
         assertHitCount(resp, 4);
+        SearchHits hits = resp.getHits();
         logger.info("{} {}", hits.getAt(0).getId(), hits.getAt(0).getScore());
         logger.info("{} {}", hits.getAt(1).getId(), hits.getAt(1).getScore());
         logger.info(DEBUG_SEP);
         assertOrderedSearchHits(resp, "1", "4");
+    }
 
-        // Small rescoring window
-        resp = client().prepareSearch()
+    public void testRescoringWithSmallRescoreWindow() throws IOException {
+        createIndexAndPopulateDocs();
+
+        SearchResponse resp = client().prepareSearch()
                 .setQuery(queryBuilder)
                 .setRescorer(
                         new GroupingMixupRescorerBuilder(
@@ -221,11 +195,52 @@ public class GroupingMixupRescorerIT extends ESIntegTestCase {
                 .execute()
                 .actionGet();
         assertHitCount(resp, 4);
+        SearchHits hits = resp.getHits();
         logger.info("{} {}", hits.getAt(0).getId(), hits.getAt(0).getScore());
         logger.info("{} {}", hits.getAt(1).getId(), hits.getAt(1).getScore());
         logger.info("{} {}", hits.getAt(2).getId(), hits.getAt(2).getScore());
         logger.info("{} {}", hits.getAt(3).getId(), hits.getAt(3).getScore());
         logger.info(DEBUG_SEP);
         assertOrderedSearchHits(resp, "1", "4", "3", "2");
+    }
+
+    private void createIndexAndPopulateDocs() throws IOException {
+        assertAcked(prepareCreate("test")
+                .setSettings(Settings.builder().put(SETTING_NUMBER_OF_SHARDS, 1))
+                .addMapping("product",
+                        jsonBuilder().startObject().startObject("product").startObject("properties")
+                                .startObject("name")
+                                .field("type", "text")
+                                .field("analyzer", "whitespace")
+                                .endObject()
+                                .startObject("company_id")
+                                .field("type", "integer")
+                                .endObject()
+                                .endObject().endObject().endObject()));
+
+        client().prepareIndex("test", "product", "1")
+                .setSource(
+                        "name", "the quick brown fox",
+                        "company_id", 1)
+                .execute()
+                .actionGet();
+        client().prepareIndex("test", "product", "2")
+                .setSource(
+                        "name", "the quick lazy huge fox jumps over the tree",
+                        "company_id", 2)
+                .execute()
+                .actionGet();
+        client().prepareIndex("test", "product", "3")
+                .setSource(
+                        "name", "quick huge brown fox",
+                        "company_id", 1)
+                .execute()
+                .actionGet();
+        client().prepareIndex("test", "product", "4")
+                .setSource("name", "the quick lonely fox")
+                .execute()
+                .actionGet();
+        ensureYellow();
+        refresh();
     }
 }
