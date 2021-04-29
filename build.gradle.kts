9cd41d87fe438228c47538b38955542dfbea210b
Alexander Koval <kovalidis@gmail.com>
2021-04-29 13:59:59 +0300

    Bintray sunset

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,4 @@
-import java.util.Date
-import com.jfrog.bintray.gradle.BintrayExtension
 import org.elasticsearch.gradle.VersionProperties
-import com.jfrog.bintray.gradle.tasks.RecordingCopyTask
 
 buildscript {
     val esVersion = project.properties["esVersion"]
@@ -21,7 +18,6 @@ buildscript {
 plugins {
     java
     idea
-    id("com.jfrog.bintray") version "1.8.4"
 }
 
 apply {
@@ -52,37 +48,3 @@ project.setProperty("noticeFile", project.rootProject.file("NOTICE.txt"))
 
 val versions = VersionProperties.getVersions() as Map<String, String>
 project.version = "$pluginVersion-es${versions["elasticsearch"]}"
-
-bintray {
-    user = if (hasProperty("bintrayUser")) {
-        property("bintrayUser").toString()
-    } else {
-        System.getenv("BINTRAY_USER")
-    }
-    key = if (hasProperty("bintrayApiKey")) {
-        property("bintrayApiKey").toString()
-    } else {
-        System.getenv("BINTRAY_API_KEY")
-    }
-    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
-        repo = "elasticsearch"
-        name = project.name
-        userOrg = "evo"
-        setLicenses("Apache-2.0")
-        setLabels("elasticsearch-plugin", "rescore-grouping-mixup")
-        vcsUrl = "https://github.com/anti-social/elasticsearch-rescore-grouping-mixup.git"
-        version(delegateClosureOf<BintrayExtension.VersionConfig> {
-            name = pluginVersion
-            released = Date().toString()
-            vcsTag = "v$pluginVersion"
-        })
-    })
-    filesSpec(delegateClosureOf<RecordingCopyTask> {
-        val distributionsDir = buildDir.resolve("distributions")
-        from(distributionsDir)
-        include("*-$pluginVersion-*.zip")
-        into(".")
-    })
-    publish = true
-    dryRun = hasProperty("bintrayDryRun")
-}
